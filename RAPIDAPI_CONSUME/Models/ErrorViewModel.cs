87a59c3816aa95576f24196ef4cb18ee73a255fd
bsarp-bs <bicakcisarp@gmail.com>
2026-04-03 12:12:42 +0300

    changes_rapidapi

--- RAPIDAPI_CONSUME/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RAPIDAPI_CONSUME.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
